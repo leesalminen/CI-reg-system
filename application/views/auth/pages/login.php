8b0144cd7f2bb50a6283fd39972f944296c23063
Lee Salminen <me@leesalminen.com>
2013-01-23 12:51:50 -0700

    v.1.1.5a
    General Sync-Up

--- application/views/auth/pages/login.php
+++ application/views/auth/pages/login.php
@@ -1,15 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="<?=base_url()?>css/main.css" type="text/css" />
+ <script src="http://campus.zoodleweb.com/assets/grocery_crud/js/jquery-1.8.2.min.js"></script></head>
+ <body style="background:url('<?php echo base_url(); ?>img/bg.jpg') top center no-repeat #000000 !important;font-family:helvetica;">
+ 
 <div id="login">
 	
-	<h2>Login to Campus Linc WebApp</h2>
+	
 	<div class="box">
-			<?php echo form_open(); ?>
+	<p style="text-align:center;padding-right:35px;"><img src="/images/logo.jpg" /></p>
+			<?php $attr = array('style' => 'margin:0 auto;margin-left:30px;'); echo form_open('',$attr); ?>
 			Username/Email:<br />
-			<input type="text" name="username" value="<?php echo set_value('username'); ?>" size="50" class="form" /><?php echo form_error('username'); ?><br /><br />
+			<input placeholder="Username/Email" type="text" name="username" value="<?php echo set_value('username'); ?>" size="50" class="form" /><?php echo form_error('username'); ?><br /><br />
 			Password:<br />
-			<input type="password" name="password" value="<?php echo set_value('password'); ?>" size="50" class="form" /><?php echo form_error('password'); ?><br /><br />
+			<input placeholder="Password" type="password" name="password" value="<?php echo set_value('password'); ?>" size="50" class="form" /><?php echo form_error('password'); ?><br /><br />
 			<input type="submit" value="Login" name="login" />
 			</form>
 	</div>
 	
-	<h4>Forgot your username and/or password? <a href="mailto:lee@zoodlemarketing.com">Click to Email Lee.</a></h4>
+	<h4 style="text-align:center;">Forgot your username and/or password?<br> <a href="mailto:lee@zoodlemarketing.com">Click to Email Lee.</a></h4>
 </div>
+</body>
+</html>
